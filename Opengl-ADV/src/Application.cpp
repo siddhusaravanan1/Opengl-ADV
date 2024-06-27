7f9c7fec15a9d06e88dc83a85ef9897f5ac61a1e
Siddharth S <siddharth.s@ubisoft.com>
2024-06-27 13:24:51 +0530

    stencil test and depth test

--- Opengl-ADV/src/Application.cpp
+++ Opengl-ADV/src/Application.cpp
@@ -87,39 +87,14 @@ std::string cubeFragmentShader =
 "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\n"
 ;
-
-std::string cube1VertexShader =
-"#version 330 core\n"
-
-"layout(location = 0) in vec3 aPosition;\n"
-"layout(location = 1) in vec3 aNormal;\n"
-"layout(location = 2) in vec2 aTexCoord;\n"
-
-"out vec3 normals;\n"
-"out vec2 texCoords;\n"
-
-"uniform mat4 model;\n"
-"uniform mat4 view;\n"
-"uniform mat4 projection;\n"
-
-"void main()\n"
-"{\n"
-"   gl_Position = projection * view * model * vec4(aPosition, 1.0f);\n"
-"   normals = aNormal;\n"
-"   texCoords = aTexCoord;\n"
-"}\n"
-;
 std::string cube1FragmentShader =
 "#version 330 core\n"
 
 "out vec4 FragColor;\n"
 
-"in vec3 normals;\n"
-"in vec2 texCoords;\n"
-
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);\n"
+"   FragColor = vec4(0.04f, 0.28f, 0.26f, 1.0f);\n"
 "}\n"
 ;
 int main(void)
@@ -145,8 +120,11 @@ int main(void)
         std::cout << "Error" << std::endl;
     }
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
     unsigned int cubeProgram = CreateShader(cubeVertexShader, cubeFragmentShader);
-    unsigned int cube1Program = CreateShader(cube1VertexShader, cube1FragmentShader);
+    unsigned int cube1Program = CreateShader(cubeVertexShader, cube1FragmentShader);
     float vertices[] = {
         //positions           //normals            //texCoords
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
@@ -223,12 +201,13 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
+        //cube render
         glUseProgram(cubeProgram);
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, 0.5f * (float)glfwGetTime() * glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(-0.5f, 0.0f, 0.0f));
+        model = glm::rotate(model, 0.5f * (float)glfwGetTime() * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         model = glm::scale(model, glm::vec3(0.5f));
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), 640.0f / 480.0f, 0.1f, 100.0f);
@@ -237,20 +216,52 @@ int main(void)
         glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
+        glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+        model = glm::rotate(model, 0.5f * (float)glfwGetTime() * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+        model = glm::scale(model, glm::vec3(0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        float boxSize = 0.6f;
+
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilMask(0xFF);
+
         glUseProgram(cube1Program);
-        glBindVertexArray(VAO1);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-0.5f, 0.0f, 0.0f));
+        model = glm::rotate(model, 0.5f * (float)glfwGetTime() * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(boxSize));
+        glUniformMatrix4fv(glGetUniformLocation(cube1Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(cube1Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(cube1Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
         glUniformMatrix4fv(glGetUniformLocation(cube1Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(cube1Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         model = glm::mat4(1.0f);
-        model = glm::rotate(model, 0.5f * (float)glfwGetTime() * glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(0.5f));
+        model = glm::rotate(model, 0.5f * (float)glfwGetTime() * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+        model = glm::scale(model, glm::vec3(boxSize));
         glUniformMatrix4fv(glGetUniformLocation(cube1Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
+
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
